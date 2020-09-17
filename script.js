ddbb650be73c51fb6f3dac54eff22b7086ea6fbf
unknown <shubhamjadhav1629@gmail.com>
2020-09-17 15:01:21 +0530

    version 1

---
+++ script.js
@@ -0,0 +1,34 @@
+let counter = 0;
+let defaultSize = 10;
+let resCounter = document.querySelector("#res-counter");
+
+function getSize(length) {
+    return defaultSize - (length / 2);
+}
+
+function buttonHandler(action) {
+    switch (action) {
+        case 'inc':
+            counter += 10;
+            break;
+        case 'dec':
+            if (counter)
+                counter--;
+            break;
+        case 'res':
+            counter = 0;
+            break;
+        default:
+    }
+}
+
+
+document.querySelectorAll("button").forEach(element => {
+    element.onclick = function () {
+        buttonHandler(element.dataset.action);
+        resCounter.innerHTML = counter;
+        resCounter.style.fontSize = getSize(("" + counter).length) + "rem";
+    }
+});
+
+
